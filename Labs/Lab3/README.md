4731da34f62c9a8d246ca396af879c0bb538d7b5
Will Ross <wross9959@gmail.com>
2024-02-08 12:19:19 -0400

    updates

--- Labs/Lab3/README.md
+++ Labs/Lab3/README.md
@@ -128,22 +128,23 @@ In c when you increment a pointer it moves to the next element of its data type 
 ## Exercise 2
 
 ### source code
-```
 // arithmetic1.c
 #include <stdio.h>
 #include <stdlib.h>
 
 void printArray(int arr[], int len){
     int *aptr = arr;
+    printf("Index\tValue\tAddress\t\t\tValue\n");
     for(int i = 0; i < len; i++){
-        printf("%d\t%d\t%p\t%d\n", i, arr[i], (void *)&arr[i], *aptr);
+        printf("%d\t%d\t%p\t%d\n", i, arr[i], &arr[i], *aptr++);
 
     }
 }
 
+
 int main (int argc ,char * * argv)
 {
-    for exercise 2
+    //for exercise 2
     int arr[] = {10, 11, 12, 13, 14, 15, 16};
     int len = sizeof(arr) / sizeof(int);
     printArray(arr, len);
--- Labs/Lab3/arrPrint.c
+++ Labs/Lab3/arrPrint.c
@@ -4,8 +4,9 @@
 
 void printArray(int arr[], int len){
     int *aptr = arr;
+    printf("Index\tValue\tAddress\t\t\tValue\n");
     for(int i = 0; i < len; i++){
-        printf("%d\t%d\t%p\t%d\n", i, arr[i], (void *)&arr[i], *aptr);
+        printf("%d\t%d\t%p\t%d\n", i, arr[i], &arr[i], *aptr++);
 
     }
 }
@@ -18,14 +19,14 @@ int arrindex(int a[], int * p){
 int main (int argc ,char * * argv)
 {
     //for exercise 2
-    // int arr[] = {10, 11, 12, 13, 14, 15, 16};
-    // int len = sizeof(arr) / sizeof(int);
-    // printArray(arr, len);
+    int arr[] = {10, 11, 12, 13, 14, 15, 16};
+    int len = sizeof(arr) / sizeof(int);
+    printArray(arr, len);
 
     //for exercise 3
-    int arr[] = {10, 11, 12, 13, 14, 15, 16};
-    for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i ++){
-        printf ("%d\t%p\n", i, arrindex( arr, & arr[i]));
-    }
+    // int arr[] = {10, 11, 12, 13, 14, 15, 16};
+    // for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i ++){
+    //     printf ("%d\t%p\n", i, arrindex( arr, & arr[i]));
+    // }
     return EXIT_SUCCESS;
 }
